671c634e3953697ae4649e3b2f48d57b126bd777
Michaux Kelley <michauxkelley@gmail.com>
2023-07-02 02:54:31 -0400

    Configure express mostly

--- server/config/express.ts
+++ server/config/express.ts
@@ -17,111 +17,117 @@ import expressFileUpload from 'express-fileupload';
 import APIError from '../app/helpers/APIError.js';
 
 const app = express();
-// const cfg = await config;
 
-// const {
-//   api: { title, version, description },
-// } = cfg;
+const {
+  api: { title, version, description },
+} = config.defaults;
 
-// const swaggerDefinition = {
-//   info: { title, version, description },
-//   host: `${cfg.host}:${cfg.port}`,
-// };
+const swaggerDefinition = {
+  info: { title, version, description },
+  host: `${config.default.host}:${config.default.port}`,
+};
 
-// const options = {
-//   swaggerDefinition,
-//   apis: ['./app/routes/api/**/*.js'],
-// };
+const options = {
+  swaggerDefinition,
+  apis: ['./app/routes/api/**/*.ts'],
+};
 
-// const swaggerSpec = swaggerJSDoc(options);
+const swaggerSpec = swaggerJSDoc(options);
 
-// app.get('/swagger.json', function (req, res) {
-//   res.setHeader('Content-Type', 'application/json');
-//   res.send(swaggerSpec);
-// });
+app.get('/swagger.json', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 
-// if (cfg.env === 'development') {
-//   app.use(logger('dev'));
-// }
+if (config.default.env === 'development') {
+  app.use(logger('dev'));
+}
 
-// // parse body params and attache them to req.body
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(compress());
-// app.use(methodOverride());
+app.use(compress());
+app.use(methodOverride());
 
 // // secure apps by setting various HTTP headers
-// app.use(helmet());
+app.use(helmet());
 
-// // enable detailed API logging in dev env
-// if (cfg.env === 'development') {
-//   expressWinston.requestWhitelist.push('body');
-//   expressWinston.responseWhitelist.push('body');
-//   app.use(
-//     expressWinston.logger({
-//       winstonInstance,
-//       meta: true, // optional: log meta data about request (defaults to true)
-//       msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
-//       bodyBlacklist: ['password', 'oldPassword', 'newPassword', 'confirmPassword'],
-//     })
-//   );
-// }
+// enable detailed API logging in dev env
+if (config.default.env === 'development') {
+  expressWinston.requestWhitelist.push('body');
+  expressWinston.responseWhitelist.push('body');
+  app.use(
+    expressWinston.logger({
+      winstonInstance,
+      meta: true, // optional: log meta data about request (defaults to true)
+      msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+      bodyBlacklist: ['pass', 'user', 'password', 'oldPassword', 'newPassword', 'confirmPassword'],
+    })
+  );
+}
 
-// const corsOptions = {
-//   origin(origin: any, callback: any) {
-//     callback(null, true);
-//   },
-//   credentials: true,
-// };
-// app.use(cors(corsOptions));
+const corsOptions = {
+  origin(origin: any, callback: any) {
+    callback(null, true);
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-// var allowCrossDomain = function (req: Request, res: Response, next: NextFunction) {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type,token');
-//   next();
-// };
-// app.use(allowCrossDomain);
-// app.use(cookieParser());
+const allowCrossDomain = function (req: Request, res: Response, next: NextFunction) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,token');
+  next();
+};
+
+app.use(allowCrossDomain);
+app.use(cookieParser());
 // app.use(
 //   expressFileUpload({
-//     limits: { fileSize: cfg.fileSizeLimit },
+//     limits: { fileSize: config.defaults.fileSizeLimit },
 //     safeFileNames: true,
 //     preserveExtension: 4,
 //     abortOnLimit: true,
 //   })
 // );
 
-// // In production serve static files using NGINX, but in development let express serve them.
-// if (cfg.env === 'development') {
+// In production serve static files using NGINX, but in development let express serve them.
+// if (config.default.env === 'development') {
 //   app.use(express.static(path.join(__dirname, '..', 'public')));
 // }
 
-// // Mount api routes on /api/v1 path.
-// app.use('/api/v1', apiRouter);
+// Mount api routes on /api/v1 path.
+app.use('/api/v1', apiRouter);
 
 // // catch 404 and forward to error handler
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//   const err = new APIError('API not found', httpStatus.NOT_FOUND);
-//   return next(err);
-// });
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const err = new APIError('API not found', httpStatus.NOT_FOUND);
+  return next(err);
+});
 
-// // log error in winston transports except when executing test suite
-// if (cfg.env !== 'test') {
-//   app.use(
-//     expressWinston.errorLogger({
-//       winstonInstance,
-//     })
-//   );
-// }
+// // Log error in winston transports except when executing test suite
+if (config.default.env !== 'test') {
+  app.use(
+    expressWinston.errorLogger({
+      winstonInstance,
+    })
+  );
+}
 
-// // error handler, send stacktrace only during development
-// // app.use((err, req, res, next) => // eslint-disable-line no-unused-vars
-// //   res.status(err.status).json({
-// //     message: err.isPublic ? err.message : httpStatus[err.status],
-// //     stack: config.env === 'development' ? err.stack : {}
-// //   })
-// // );
+// error handler, send stacktrace only during development
+app.use(
+  (
+    err: APIError,
+    _req: Request,
+    res: Response,
+    _next: NextFunction // eslint-disable-line no-unused-vars
+  ) =>
+    res.status(err.status).json({
+      message: err.isPublic ? err.message : httpStatus[err.status],
+      stack: config.default.env === 'development' ? err.stack : {},
+    })
+);
 
 export default app;
