61b6852adb18cad41c85fd55157405922f992e62
Michaux Kelley <michauxkelley@gmail.com>
2023-07-04 00:08:18 -0400

    Cleanup

--- server/config/express.ts
+++ server/config/express.ts
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'development') {
 
   const swaggerSpec = swaggerJSDoc(options);
 
-  app.get('/swagger.json', function (req, res) {
+  app.get('/swagger.json', function (_req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send(swaggerSpec);
   });
