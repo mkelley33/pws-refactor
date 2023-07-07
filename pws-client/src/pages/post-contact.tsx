00cecc6604c2ee35f61fea1ddc759ed4e6a82a83
Michaux Kelley <michauxkelley@gmail.com>
2023-07-07 17:00:51 -0400

    Add a thank you page post contact page

---
+++ pws-client/src/pages/post-contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '@components/layout';
+
+const PostContact = () => {
+  return (
+    <Layout>
+      <h1>Thank you</h1>
+      <p>Your message has been sent.</p>
+    </Layout>
+  );
+};
+
+export default PostContact;
