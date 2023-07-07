66064503c1cf691e2e029bc5408407b850877dbd
Michaux Kelley <michauxkelley@gmail.com>
2023-07-07 11:27:56 -0400

    Convert checkbox component to tsx

---
+++ pws-client/src/components/common/forms/Checkbox.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface ICheckboxProps {
+  field: {
+    name: string;
+    onChange: () => void;
+  };
+  id: string;
+  label: string;
+  className: string;
+  value: string;
+  defaultChecked: boolean;
+}
+
+const Checkbox = ({
+  field: { name, onChange },
+  id,
+  label,
+  className,
+  ...props
+}: ICheckboxProps) => {
+  return (
+    <div>
+      <input
+        name={name}
+        id={id}
+        type='checkbox'
+        value={props.value}
+        onChange={onChange}
+        defaultChecked={props.defaultChecked}
+      />
+      <label htmlFor={id}>{label}</label>
+    </div>
+  );
+};
+
+export default Checkbox;
