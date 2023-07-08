10bf030d8e6246f4886f1a82938be84cf08d9d0b
Michaux Kelley <michauxkelley@gmail.com>
2023-07-08 13:30:14 -0400

    Refactor axios module

--- pws-client/src/api/index.ts
+++ pws-client/src/api/index.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const ax = axios.create({
+export default axios.create({
   baseURL: `${process.env.GATSBY_API_URL}/api/v1`,
   withCredentials: true,
 });
-
-export default ax;
