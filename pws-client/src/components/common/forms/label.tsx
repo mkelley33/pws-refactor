034996cf480d302429770a4fadda1d55e1b17f68
Michaux Kelley <michauxkelley@gmail.com>
2023-07-08 15:47:40 -0400

    Refactor Label component

--- pws-client/src/components/common/forms/label.tsx
+++ pws-client/src/components/common/forms/label.tsx
@@ -1,12 +1,2 @@
-import React from 'react';
-
-// TODO: replace any with a better interface
-const Label = ({ className, children, ...props }: any) => {
-  return (
-    <label className={className} {...props}>
-      {children}
-    </label>
-  );
-};
-
+const Label = ({ children, ...props }: any) => <label {...props}>{children}</label>;
 export default Label;
