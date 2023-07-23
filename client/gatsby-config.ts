c5b57914e7d14883fae07bf65c55e8311567e6f9
Michaux Kelley <michauxkelley@gmail.com>
2023-07-23 19:35:19 -0400

    Add correct ga id

--- client/gatsby-config.ts
+++ client/gatsby-config.ts
@@ -27,7 +27,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          'G-ZHK22KF3ZV', // Google Analytics / GA
+          'G-2WJHGXH0MB', // Google Analytics / GA
           // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
