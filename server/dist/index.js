c40a088fd5808c5008036f39e0827fe6f79f87a3
Michaux Kelley <michauxkelley@gmail.com>
2023-08-01 15:17:16 -0400

    Remove mongo

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,10 +1,7 @@
-import mongoose from 'mongoose';
-import util from 'util';
 import config from './config/env/index.js';
 import app from './config/express.js';
 import debugging from 'debug';
 import dotenv from 'dotenv';
-import Promise from 'bluebird';
 import http from 'http';
 if (config.default.env === 'development')
     dotenv.config({ path: './.env.development' });
@@ -14,18 +11,17 @@ else
     dotenv.config({ path: './.env' });
 const debug = debugging('index');
 // Use bluebird promises in mongoose
-mongoose.Promise = Promise;
-mongoose.connect(config.default.db.uri, config.default.db.options);
-mongoose.connection.on('error', () => {
-    if (config.default.env !== 'production')
-        throw new Error(`Unable to connect to database: ${config.default.db.uri}`);
-});
-// Print mongoose logs if this is set to true
-if (config.default.mongoose.debug) {
-    mongoose.set('debug', (collectionName, method, query, doc) => {
-        debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
-    });
-}
+// mongoose.Promise = Promise;
+// mongoose.connect(config.default.db.uri, config.default.db.options);
+// mongoose.connection.on('error', () => {
+//   if (config.default.env !== 'production') throw new Error(`Unable to connect to database: ${config.default.db.uri}`);
+// });
+// // Print mongoose logs if this is set to true
+// if (config.default.mongoose.debug) {
+//   mongoose.set('debug', (collectionName, method, query, doc) => {
+//     debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
+//   });
+// }
 const { host, port } = config.default.server;
 if (process.env.NODE_ENV === 'production')
     app.listen(process.env.PORT || 5001);
