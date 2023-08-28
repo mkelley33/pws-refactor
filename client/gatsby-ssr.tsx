7156574decf0f8faf51149e11c836f04c2d5d0ea
Michaux Kelley <michauxkelley@gmail.com>
2023-08-28 00:35:09 -0400

    Wrap with theme provider ssr

--- client/gatsby-ssr.tsx
+++ client/gatsby-ssr.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@components/theme-context';
 
 import store from './src/store';
 export const wrapRootElement = ({ element }: { element: JSX.Element }) => {
-  return <Provider store={store}>{element}</Provider>;
+  return (
+    <Provider store={store}>
+      <ThemeProvider>{element}</ThemeProvider>
+    </Provider>
+  );
 };
 
 export const onRenderBody = ({ setHtmlAttributes }: { setHtmlAttributes: (lang: { lang: string }) => void }) => {
